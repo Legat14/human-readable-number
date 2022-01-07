006fd8a770833db067b78300926e701f8bfd1639
Legat <zia-d@mail.ru>
2022-01-07 18:05:36 +0400

    feat: make 821 maches

--- src/index.js
+++ src/index.js
@@ -1,58 +1,95 @@
 module.exports = function toReadable(number) {
 
-  let stringNumber = number.toString();
-  let arrayNumber = stringNumber.split('');
+    let stringNumber = number.toString();
+    let arrayNumber = stringNumber.split('');
 
-  arrayNumber.forEach((element, i) => {
-      switch (element) {
-          case '0':
-              arrayNumber[i] = 'zero';
-              break;
-          case '1':
-              arrayNumber[i] = 'one';
-              break;
-          case '2':
-              arrayNumber[i] = 'two';
-              break;
-          case '3':
-              arrayNumber[i] = 'three';
-              break;
-          case '4':
-              arrayNumber[i] = 'four';
-              break;
-          case '5':
-              arrayNumber[i] = 'five';
-              break;
-          case '6':
-              arrayNumber[i] = 'six';
-              break;
-          case '7':
-              arrayNumber[i] = 'seven';
-              break;
-          case '8':
-              arrayNumber[i] = 'eight';
-              break;
-          case '9':
-              arrayNumber[i] = 'nine';
-              break;
-      }
-  });
+    // Заменяем все цифры словами, обозначающими цифры
 
-  if (arrayNumber.length === 3) {
-    arrayNumber.unshift(arrayNumber[0]);
-    arrayNumber[1] = 'hundred';
-  }
+    arrayNumber.forEach((element, i) => {
+        switch (element) {
+            case '0':
+                arrayNumber[i] = 'zero';
+                break;
+            case '1':
+                arrayNumber[i] = 'one';
+                break;
+            case '2':
+                arrayNumber[i] = 'two';
+                break;
+            case '3':
+                arrayNumber[i] = 'three';
+                break;
+            case '4':
+                arrayNumber[i] = 'four';
+                break;
+            case '5':
+                arrayNumber[i] = 'five';
+                break;
+            case '6':
+                arrayNumber[i] = 'six';
+                break;
+            case '7':
+                arrayNumber[i] = 'seven';
+                break;
+            case '8':
+                arrayNumber[i] = 'eight';
+                break;
+            case '9':
+                arrayNumber[i] = 'nine';
+                break;
+        }
+    });
 
-  if (arrayNumber[arrayNumber.length - 1] === 'zero' && arrayNumber[arrayNumber.length - 2] == 'one') {
-    arrayNumber.pop();
-    arrayNumber[arrayNumber.length - 1] = 'ten';
-  }
+    // Заменяем десятки (кроме одного десятка)
 
-  arrayNumber.forEach((element, i) => {
-    if(element === 'zero' && arrayNumber.length !== 1) {
-      arrayNumber.splice(i, 1);
+    switch (arrayNumber[1]) {
+        case 'two':
+            arrayNumber[1] = 'twenty';
+            break;
+        case 'three':
+            arrayNumber[1] = 'thirty';
+            break;
+        case 'four':
+            arrayNumber[1] = 'forty';
+            break;
+        case 'five':
+            arrayNumber[1] = 'fifty';
+            break;
+        case 'six':
+            arrayNumber[1] = 'sixty';
+            break;
+        case 'seven':
+            arrayNumber[1] = 'seventy';
+            break;
+        case 'eight':
+            arrayNumber[1] = 'eighty';
+            break;
+        case 'nine':
+            arrayNumber[1] = 'ninety';
+            break;
     }
-  });
 
-  return arrayNumber.join(' ');
+    // Добавляем слово сто
+
+    if (arrayNumber.length === 3) {
+        arrayNumber.unshift(arrayNumber[0]);
+        arrayNumber[1] = 'hundred';
+    }
+
+    // Заменяем один и ноль на десять
+
+    if (arrayNumber[arrayNumber.length - 1] === 'zero' && arrayNumber[arrayNumber.length - 2] == 'one') {
+        arrayNumber.pop();
+        arrayNumber[arrayNumber.length - 1] = 'ten';
+    }
+
+    // Убираем непроизносящиеся нули
+
+    arrayNumber.forEach((element, i) => {
+        if (element === 'zero' && arrayNumber.length !== 1) {
+            arrayNumber.splice(i, 1);
+        }
+    });
+
+    return arrayNumber.join(' ');
 }
